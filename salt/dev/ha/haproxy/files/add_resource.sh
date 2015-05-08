ae2b9166335f89907a1552b286e6d2ec2945a802
XiaBing Yao <yao.xiabing@99cloud.net>
2015-05-08 05:01:53 -0400

    remove the conflict file
    remove the conflict file, because I have been moved these file to other path.
    
    Change-Id: I5b872ec6bbef1a4365f9baa2f1cf33b2a162202f

--- salt/dev/ha/haproxy/files/add_resource.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-crm configure primitive HAPROXY service:haproxy \
-        meta target-role=Started \
-        op monitor interval=20s
-
-crm configure colocation vip-with-haproxy inf: VIP HAPROXY
-crm configure order vip-before-haproxy Mandatory: VIP HAPROXY
